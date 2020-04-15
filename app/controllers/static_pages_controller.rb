4bc83be36493ae20cbe93d21030a2c916839eaf3
Joan Currie <jecurrie8@gmail.com>
2020-04-14 22:45:52 -0400

    Ready to deploy v1.0

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end  
+
+  def project
+  end 
+
+  def resume
+  end    
+
+  def contact
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/project',  to: 'static_pages#project'
+  get '/resume',   to: 'static_pages#resume'
+  get '/contact',  to: 'static_pages#contact'
+end
